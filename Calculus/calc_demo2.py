c2c70729f6dbdef82c678f4e84c510d2f2827730
Jim Vargas <jimball1@hotmail.com>
2017-09-29 21:46:13 -0700

    limits

---
+++ Calculus/calc_demo2.py
@@ -0,0 +1,29 @@
+import sympy as sympy
+
+'''START:
+Limits'''
+
+sympy.init_printing()
+
+a, b, x, y, z = sympy.symbols('a b x y z')
+
+sympy.Limit(sympy.sin(x) / x, x, 0)
+#(symbolic) (expression, variable of limit, 
+#               what the variable of the limit is apprpaching)
+
+sympy.limit(sympy.sin(x) / x, x, 0)
+# 1
+
+sym.Limit(1/x, x, sympy.oo)
+# lim_x->infinity [a/x]
+
+sym.Limit(1./x, x, 0, dir='+')
+# limit from the positive side
+# one is a float
+
+sym.Limit(1./x, x, -sympy.oo, dir='-').doit()
+# limit to -infinity from the negative side
+
+'''END:
+Limits'''
+
